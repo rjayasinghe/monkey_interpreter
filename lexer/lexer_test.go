0115d67f54f706d294bf8ae5add29dbc0feb0a2e
Robin <robin@jayasinghe.de>
2022-01-07 20:33:33 +0000

    touch

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/rjayasinghe/monkey_interpreter/token"
 )
 
-func TestNextToken(t *testing.T) {
+func TestNextTokenSimpleInput(t *testing.T) {
 
 	input := `=+(){},;`
 
@@ -23,6 +23,48 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got %q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got %q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+func TestNextTokenSimpleSourceCode(t *testing.T) {
+
+	input := `
+		let five = 5;
+		let ten = 10;
+		let add = fn(x,y) {
+			x + y;
+		};
+
+		let result = add(five, ten);
+	`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
 	}
 
 	l := New(input)
